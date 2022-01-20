eb3050eeb9e2f5200bb7fe2741a9a293596f7d21
이도현 <ksmfou98@naver.com>
2022-01-20 21:41:53 +0900

    Feat: FolderList Query hooks 구현

---
+++ web/src/hooks/folder/useFolderListQuery.ts
@@ -0,0 +1,19 @@
+import { getFolderListAPI } from "lib/api/folder";
+import { ReactQueryKey } from "lib/queryKey";
+import { useQuery } from "react-query";
+
+export type FolderQueryKindTypes = "sidebar" | "modal" | "layer";
+
+export default function useFolderListQuery(kind: FolderQueryKindTypes) {
+  const query = useQuery(
+    ReactQueryKey.folderList(kind),
+    () => getFolderListAPI(),
+    {
+      cacheTime: 5 * 60 * 1000,
+      staleTime: 5 * 60 * 1000,
+      retry: false,
+    }
+  );
+
+  return query;
+}
