6dea3fff1304d88b093521f5a64acf1cead83937
이도현 <ksmfou98@naver.com>
2022-01-26 20:25:45 +0900

    Feat: accessToken 만료되었을 때 해야 할일 주석 작성

--- web/src/lib/api/client.ts
+++ web/src/lib/api/client.ts
@@ -28,4 +28,42 @@ client.interceptors.request.use(
   }
 );
 
+client.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  async (error) => {
+    const {
+      response: { status, data },
+    } = error;
+
+    console.log(data);
+    // @TODO 지환님이 에러 메세지 형식 다 맞춰주시면 이쪽
+    // switch 문을 data로 바꿔서 각 에러메세지 별로 처리하도록 해야함.
+    // accessToken이 만료되었을 때, refreshToken이 만료되었을 때 등등
+    switch (status) {
+      case 401:
+        // window.location.replace("/login");
+        // const tokens = getTokens();
+        // if (!tokens) throw new Error("No tokens found");
+        // const { accessToken, refreshToken } = tokens;
+        // console.log(tokens);
+        // const { data } = await axios.get(
+        //   `${SERVER_URL}/api/v1/user/reIssuanceAccessToken`,
+        //   {
+        //     headers: {
+        //       accessToken: `Bearer ${accessToken}`,
+        //       refreshToken: `Bearer ${refreshToken}`,
+        //     },
+        //   }
+        // );
+
+        break;
+
+      default:
+        throw new Error("Unknown Error");
+    }
+  }
+);
+
 export default client;
