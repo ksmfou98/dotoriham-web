db8e003065d687111d29443b0a594780b52c879e
이도현 <ksmfou98@naver.com>
2022-02-13 21:38:02 +0900

    feat: MyPage Profile 영역 컴포넌트 생성

---
+++ src/components/mypage/MyPageProfile.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import MyPageHead from "./MyPageHead";
+
+function MyPageProfile() {
+  return (
+    <>
+      <MyPageHead headText="프로필" />
+    </>
+  );
+}
+
+export default MyPageProfile;
--- src/pages/MyPage.tsx
+++ src/pages/MyPage.tsx
@@ -1,10 +1,11 @@
+import MyPageProfile from "components/mypage/MyPageProfile";
 import MyPageTemplate from "components/mypage/MyPageTemplate";
 import React from "react";
 
 function MyPage() {
   return (
     <MyPageTemplate>
-      <div>MyPage</div>
+      <MyPageProfile />
     </MyPageTemplate>
   );
 }
