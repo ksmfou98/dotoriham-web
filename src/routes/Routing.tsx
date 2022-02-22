b38bd5fd255de66884eb0b292e77287f1a31f2d0
이도현 <ksmfou98@naver.com>
2022-02-23 07:03:35 +0900

    fix: Home Route 제거

--- src/routes/Routing.tsx
+++ src/routes/Routing.tsx
@@ -1,13 +1,12 @@
 import React from "react";
 import DotoriPage from "pages/DotoriPage";
-import LandingPage from "pages/LandingPage";
 import LoginPage from "pages/LoginPage";
 import MyPage from "pages/MyPage";
 import NotFoundPage from "pages/NotFoundPage";
 import SearchPage from "pages/SearchPage";
 import SignUpPage from "pages/SignUpPage";
 import TrashPage from "pages/TrashPage";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Path from "./Path";
 import SidebarRouteLayout from "./SidebarRouteLayout";
 
@@ -16,7 +15,10 @@ function Routing() {
     <Routes>
       <Route path={Path.LoginPage} element={<LoginPage />} />
       <Route path={Path.SignUpPage} element={<SignUpPage />} />
-      <Route path={Path.HomePage} element={<LandingPage />} />
+      <Route
+        path={Path.HomePage}
+        element={<Navigate replace to={Path.DotoriPage} />}
+      />
       <Route path={Path.MyPage} element={<MyPage />} />
 
       <Route path={Path.HomePage} element={<SidebarRouteLayout />}>
