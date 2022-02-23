b6868aa69e64af9713ccddb6b887db3a2f2ae8f8
이도현 <64634992+ksmfou98@users.noreply.github.com>
2022-02-23 16:15:30 +0900

    feat: DotoriFilterMenu Template

--- src/components/dotori/DotoriFilterMenu.tsx
+++ src/components/dotori/DotoriFilterMenu.tsx
@@ -1,7 +1,27 @@
 import React from "react";
+import styled from "styled-components";
+import dotoriFilterMenus from "./DotoriFilterMenus";
 
 function DotoriFilterMenu() {
-  return <div>DotoriFilterMenu</div>;
+  return (
+    <DotoriFilterMenuBlock>
+      <MenuInner>
+        {dotoriFilterMenus.map(({ label, text }) => (
+          <MenuItem key={`MenuItem_${text}`}>
+            <ItemText>{text}</ItemText>
+          </MenuItem>
+        ))}
+      </MenuInner>
+    </DotoriFilterMenuBlock>
+  );
 }
+
+const DotoriFilterMenuBlock = styled.div``;
+
+const MenuInner = styled.div``;
+
+const MenuItem = styled.div``;
+
+const ItemText = styled.span``;
 
 export default DotoriFilterMenu;
