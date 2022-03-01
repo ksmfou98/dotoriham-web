9bb1d3f76699f95da2216e1f189cc94d2c54ec6a
이도현 <ksmfou98@naver.com>
2022-03-01 17:33:51 +0900

    feat: 쿼리 스트링 검색 구현

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,7 +1,19 @@
-import React from "react";
+import DotoriTemplate from "components/dotori/DotoriTemplate";
+import React, { useMemo } from "react";
+import qs from "qs";
+import { useLocation } from "react-router-dom";
 
 function SearchPage() {
-  return <div>SearchPage</div>;
+  const location = useLocation();
+
+  const query = useMemo(() => {
+    const parsed = qs.parse(location.search, {
+      ignoreQueryPrefix: true,
+    }) as { q: string };
+    return parsed;
+  }, [location.search]);
+
+  return <DotoriTemplate path="search" keyword={query.q} />;
 }
 
 export default SearchPage;
