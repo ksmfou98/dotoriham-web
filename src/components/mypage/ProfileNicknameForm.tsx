26bdefb0ff9416d7b2bf98eb2b82aaa89b1ca54a
이도현 <ksmfou98@naver.com>
2022-03-11 23:17:30 +0900

    feat: profile nickname form Label

--- src/components/mypage/ProfileNicknameForm.tsx
+++ src/components/mypage/ProfileNicknameForm.tsx
@@ -1,7 +1,17 @@
+import SmallBlackText from "components/common/SmallBlackText";
 import React from "react";
+import styled from "styled-components";
 
 function ProfileNicknameForm() {
-  return <div>ProfileNicknameForm</div>;
+  return (
+    <div>
+      <FormLabel width="297px" label="닉네임" />
+    </div>
+  );
 }
+
+const FormLabel = styled(SmallBlackText)`
+  padding-top: 8px;
+`;
 
 export default ProfileNicknameForm;
