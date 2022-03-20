d1be85e392b8a692cb970a6bddfabe4e464b5aeb
이도현 <ksmfou98@naver.com>
2022-03-20 22:19:13 +0900

    feat: profile nickname form input style

--- src/components/mypage/ProfileNicknameForm.tsx
+++ src/components/mypage/ProfileNicknameForm.tsx
@@ -1,14 +1,20 @@
+import Input from "components/common/Input";
 import SmallBlackText from "components/common/SmallBlackText";
 import React from "react";
 import styled from "styled-components";
 
 function ProfileNicknameForm() {
   return (
-    <div>
+    <NicknameFormBlock>
       <FormLabel width="297px" label="닉네임" />
-    </div>
+      <Input width="273px" height="36px" placeholder="닉네임을 입력해주세요" />
+    </NicknameFormBlock>
   );
 }
+
+const NicknameFormBlock = styled.div`
+  display: flex;
+`;
 
 const FormLabel = styled(SmallBlackText)`
   padding-top: 8px;
