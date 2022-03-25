4c0f7f9eb9119652e6739b9165e16671c70ab38c
이도현 <ksmfou98@naver.com>
2022-03-25 22:15:19 +0900

    feat: tutorial 전역 상태 생성

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import { combineReducers } from "redux";
 import dotori from "./dotori";
 import folder from "./folder";
+import tutorial from "./tutorial";
 import user from "./user";
 
 const rootReducer = combineReducers({
   user: user.reducer,
   dotori: dotori.reducer,
   folder: folder.reducer,
+  tutorial: tutorial.reducer,
 });
 
 const store = configureStore({
