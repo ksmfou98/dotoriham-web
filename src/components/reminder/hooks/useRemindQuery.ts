d9179ff10ddb9a5421c8deb739fcbe13066abbd2
이도현 <ksmfou98@naver.com>
2022-03-25 20:41:47 +0900

    fix: 리마인더 api 캐싱 타임 설정

--- src/components/reminder/hooks/useRemindQuery.ts
+++ src/components/reminder/hooks/useRemindQuery.ts
@@ -3,6 +3,9 @@ import { ReactQueryKey } from "lib/queryKey";
 import { useQuery } from "react-query";
 
 export default function useRemindQuery() {
-  const query = useQuery(ReactQueryKey.remindList(), () => getRemindListAPI());
+  const query = useQuery(ReactQueryKey.remindList(), () => getRemindListAPI(), {
+    cacheTime: 3 * 60 * 1000,
+    staleTime: 3 * 60 * 1000,
+  });
   return query;
 }
