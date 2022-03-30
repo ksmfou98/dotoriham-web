ce0b92f7eb2bfa64a46affda306dd3fe7f370d7c
이도현 <ksmfou98@naver.com>
2022-03-30 21:17:38 +0900

    feat: 튜토리얼 메뉴 모달 구현

--- src/components/tutorial/QuestionButton.tsx
+++ src/components/tutorial/QuestionButton.tsx
@@ -1,17 +1,31 @@
-import { QuestionIcon } from "assets/icons";
-import useToggle from "hooks/useToggle";
 import React from "react";
+import { QuestionActiveIcon, QuestionIcon } from "assets/icons";
+import useToggle from "hooks/useToggle";
+import { palette } from "lib/styles/palette";
+import transitions from "lib/styles/transitions";
 import { useSelector } from "react-redux";
 import { tutorialSelector } from "stores/tutorial";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 function QuestionButton() {
   const { isAlarm, isGuide } = useSelector(tutorialSelector);
-  const [isTutorialModal, setIsTutorialModal] = useToggle();
+  const [isTutorialMenu, onToggleTutorialMenu] = useToggle();
 
   return (
-    <QuestionButtonStyled>
-      <QuestionIcon />
+    <QuestionButtonStyled onClick={onToggleTutorialMenu}>
+      {isTutorialMenu ? (
+        <>
+          <TutorialMenuBox variant="secondary" top={-12} left={47}>
+            알림이 안 와요!
+          </TutorialMenuBox>
+          <TutorialMenuBox variant="primary" top={-60} left={47}>
+            도토리함 가이드
+          </TutorialMenuBox>
+          <QuestionActiveIcon />
+        </>
+      ) : (
+        <QuestionIcon />
+      )}
     </QuestionButtonStyled>
   );
 }
@@ -19,6 +33,42 @@ function QuestionButton() {
 const QuestionButtonStyled = styled.button`
   margin-top: 80px;
   position: relative;
+`;
+
+interface TutorialMenuStyle {
+  variant: "primary" | "secondary";
+  top: number;
+  left: number;
+}
+
+const TutorialMenuBox = styled.div<TutorialMenuStyle>`
+  width: 118px;
+  height: 42px;
+  font-size: 14px;
+  font-weight: bold;
+  font-family: "Cafe24Ssurround";
+  animation: ${transitions.fadeIn} 0.2s ease-in-out;
+  z-index: 100;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 8px 8px 8px 0px;
+  cursor: pointer;
+  position: absolute;
+  top: ${(props) => props.top}px;
+  left: ${(props) => props.left}px;
+  ${(props) =>
+    props.variant === "primary"
+      ? css`
+          background-color: ${palette.primary};
+          color: ${palette.white};
+          border: none;
+        `
+      : css`
+          background-color: ${palette.white};
+          color: ${palette.primary};
+          border: 1px solid ${palette.primary};
+        `}
 `;
 
 export default QuestionButton;
