985be3a64f8b14c334c0f3094d81984233d425c4
이도현 <64634992+ksmfou98@users.noreply.github.com>
2022-03-31 10:03:41 +0900

    fix: useInput return type readonly로 수정

--- src/hooks/useInput.ts
+++ src/hooks/useInput.ts
@@ -1,12 +1,10 @@
 import React, { useState } from "react";
 
-export default function useInput(
-  initialValue: string
-): [string, (e: React.ChangeEvent<HTMLInputElement>) => void, typeof setValue] {
+export default function useInput(initialValue: string) {
   const [value, setValue] = useState(initialValue);
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setValue(e.target.value);
   };
 
-  return [value, onChange, setValue];
+  return [value, onChange, setValue] as const;
 }
