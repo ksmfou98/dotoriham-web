bd09223fa39274529a741dcab6fa9d0ed908e195
이도현 <ksmfou98@naver.com>
2022-04-01 22:40:55 +0900

    test: setValue 값으로 직접 value를 변경할 수 있다

--- src/hooks/__test__/useInput.test.ts
+++ src/hooks/__test__/useInput.test.ts
@@ -1,4 +1,4 @@
-import { renderHook } from "@testing-library/react-hooks";
+import { renderHook, act } from "@testing-library/react-hooks";
 import { useInput } from "hooks";
 
 describe("useInput", () => {
@@ -14,9 +14,23 @@ describe("useInput", () => {
 
   test("onChange 함수로 value 값을 변경할 수 있다", () => {
     const { result } = renderHook(() => useInput(""));
-    result.current[1]({
-      target: { value: "테스트입니다" },
-    } as React.ChangeEvent<HTMLInputElement>);
+
+    act(() => {
+      result.current[1]({
+        target: { value: "테스트입니다" },
+      } as React.ChangeEvent<HTMLInputElement>);
+    });
+
     expect(result.current[0]).toBe("테스트입니다");
+  });
+
+  test("setValue 값으로 직접 value를 변경할 수 있다", () => {
+    const { result } = renderHook(() => useInput(""));
+
+    act(() => {
+      result.current[2]("두번째 테스트");
+    });
+
+    expect(result.current[0]).toBe("두번째 테스트");
   });
 });
