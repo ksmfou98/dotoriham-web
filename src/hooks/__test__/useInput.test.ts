89814596264481fbf2bf3080a3811837bed60c02
이도현 <ksmfou98@naver.com>
2022-04-01 22:37:27 +0900

    test: useInput은 길이가 3인 배열을 리턴한다. (value, onChange, setValue)

--- src/hooks/__test__/useInput.test.ts
+++ src/hooks/__test__/useInput.test.ts
@@ -2,6 +2,11 @@ import { renderHook } from "@testing-library/react-hooks";
 import { useInput } from "hooks";
 
 describe("useInput", () => {
+  test("useInput은 길이가 3인 배열을 리턴한다. (value, onChange, setValue)", () => {
+    const { result } = renderHook(() => useInput(""));
+    expect(result.current).toHaveLength(3);
+  });
+
   test("initialValue를 입력하면 value 값이 설정 된다.", () => {
     const { result } = renderHook(() => useInput("test"));
     expect(result.current[0]).toBe("test");
