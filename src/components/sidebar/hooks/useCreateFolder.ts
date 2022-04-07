313497b9bbf480ca23f35a6042eee350fed7c246
이도현 <ksmfou98@naver.com>
2022-04-07 23:32:51 +0900

    fix: 폴더 생성시 child Folder 갱신

--- src/components/sidebar/hooks/useCreateFolder.ts
+++ src/components/sidebar/hooks/useCreateFolder.ts
@@ -1,6 +1,8 @@
 import { ItemId } from "@atlaskit/tree";
 import { createFolderAPI } from "lib/api/folder";
+import { QueryKey } from "lib/queryKey";
 import { findChildrenLengthById } from "lib/utils/atlaskitTreeFinder";
+import { useQueryClient } from "react-query";
 import { useDispatch, useSelector } from "react-redux";
 import { addFolder, folderSelector } from "stores/folder";
 import { ICreateFolderRequest } from "types/folder";
@@ -8,6 +10,7 @@ import { ICreateFolderRequest } from "types/folder";
 export default function useCreateFolder() {
   const folders = useSelector(folderSelector);
   const dispatch = useDispatch();
+  const queryClient = useQueryClient();
 
   // 새로운 폴더 데이터 생성
   const createNewFolderData = (folderId: ItemId, name: string) => {
@@ -35,6 +38,7 @@ export default function useCreateFolder() {
       const { folderId } = await createFolderAPI(requestBody);
       const newFolderData = createNewFolderData(folderId, requestBody.name);
       dispatch(addFolder({ folderId, parentId, newFolderData }));
+      queryClient.invalidateQueries(QueryKey.CHILD_FOLDER_LIST);
     } catch (e) {
       console.error(e);
     }
