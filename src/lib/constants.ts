d6080b0e6cf5019f97309afe3a4cbf2f66635ac2
이도현 <ksmfou98@naver.com>
2022-04-08 19:44:33 +0900

    feat: 테스트 서버, 배포 서버 분리하기

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,3 +1,3 @@
-export const SERVER_URL = "https://dotoriham.duckdns.org";
+export const SERVER_URL = process.env.REACT_APP_API_URL;
 export const GOOGLE_CLIENT_ID =
   "291594006814-9tmu29asficp56tn7q8ronumev1s39ru.apps.googleusercontent.com";
