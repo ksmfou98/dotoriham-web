93677a964eaf5e201115ed57c4e1d21d997ed2b3
이도현 <ksmfou98@naver.com>
2022-04-10 19:44:27 +0900

    feat: not found 페이지 스타일링

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,7 +1,61 @@
+import { SymbolGray96Icon } from "assets/icons";
+import Button from "components/common/Button";
+import { palette } from "lib/styles/palette";
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
 
 function NotFoundPage() {
-  return <div>NotFoundPage</div>;
+  const navigate = useNavigate();
+
+  return (
+    <NotFoundPageBlock>
+      <NotFoundContent>
+        <SymbolGray96Icon />
+        <StrongText>404 : page not found</StrongText>
+        <Text>페이지를 찾을 수 없어요!</Text>
+      </NotFoundContent>
+      <Button
+        variant="primary"
+        width="172px"
+        height="40px"
+        onClick={() => navigate("/")}
+      >
+        홈으로
+      </Button>
+    </NotFoundPageBlock>
+  );
 }
+
+const NotFoundPageBlock = styled.div`
+  width: 100%;
+  margin-top: 273px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+
+const NotFoundContent = styled.div`
+  width: 273px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-bottom: 44px;
+`;
+
+const StrongText = styled.span`
+  font-size: 20px;
+  font-weight: 500;
+  color: ${palette.grayDarker};
+  font-family: Roboto;
+  margin: 16px 0;
+`;
+
+const Text = styled.span`
+  font-size: 14px;
+  line-height: 1.5;
+  color: ${palette.grayDarker};
+`;
 
 export default NotFoundPage;
