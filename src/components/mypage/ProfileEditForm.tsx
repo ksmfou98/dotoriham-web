101f5270d9f19c0c18cedfae6fae9cf9bf8a4fb5
이도현 <ksmfou98@naver.com>
2022-04-21 20:53:55 +0900

    refactor: 마이페이지 닉네임 입력시 랜더링 최적화

--- src/components/mypage/ProfileEditForm.tsx
+++ src/components/mypage/ProfileEditForm.tsx
@@ -4,7 +4,7 @@ import {
   uploadProfileImageAPI,
 } from "lib/api/user";
 import { palette } from "lib/styles/palette";
-import React, { useState } from "react";
+import React, { useCallback, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { userSelector } from "stores/user";
@@ -31,7 +31,7 @@ function ProfileEditForm() {
     imageFileName: DEFAULT_IMAGE_FILE_NAME,
     nickname: user.name,
   });
-  const { nickname, profileImage } = form;
+  const { nickname, profileImage, imageFileName } = form;
 
   // 닉네임 인풋 상태 변경
   const onChangeNickname = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -39,12 +39,19 @@ function ProfileEditForm() {
   };
 
   // 프로필 이미지, 이름 상태 변경
-  const onChangeProfileImage = (
-    newImg: string,
-    newFileName: string | undefined = DEFAULT_IMAGE_FILE_NAME
-  ) => {
-    setForm({ ...form, profileImage: newImg, imageFileName: newFileName });
-  };
+  const onChangeProfileImage = useCallback(
+    (
+      newImg: string,
+      newFileName: string | undefined = DEFAULT_IMAGE_FILE_NAME
+    ) => {
+      setForm((prevForm) => ({
+        ...prevForm,
+        profileImage: newImg,
+        imageFileName: newFileName,
+      }));
+    },
+    []
+  );
 
   // 닉네임 인풋에서 초점을 벗어났을 시에 액션
   const onBlurNickname = async () => {
@@ -65,30 +72,33 @@ function ProfileEditForm() {
   };
 
   // 프로필 이미지 업로드
-  const onImageUpload = async (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (e.target.files !== null) {
-      if (e.target.files[0].size > 10000000) {
-        alert("파일 용량이 10MB를 초과하였습니다.");
-        return;
-      }
+  const onImageUpload = useCallback(
+    async (e: React.ChangeEvent<HTMLInputElement>) => {
+      if (e.target.files !== null) {
+        if (e.target.files[0].size > 10000000) {
+          alert("파일 용량이 10MB를 초과하였습니다.");
+          return;
+        }
 
-      const fd = new FormData();
-      fd.append("image", e.target.files[0]);
-      try {
-        const data = await uploadProfileImageAPI(fd);
-        onChangeProfileImage(data.imageUrl, e.target.files[0].name);
-        console.log(data);
-      } catch (err) {
-        console.log(err);
-        alert("이미지 업로드에 실패했습니다.");
+        const fd = new FormData();
+        fd.append("image", e.target.files[0]);
+        try {
+          const data = await uploadProfileImageAPI(fd);
+          onChangeProfileImage(data.imageUrl, e.target.files[0].name);
+          console.log(data);
+        } catch (err) {
+          console.log(err);
+          alert("이미지 업로드에 실패했습니다.");
+        }
       }
-    }
-  };
+    },
+    [onChangeProfileImage]
+  );
 
   // 업로드 한 프로필 이미지 제거
-  const onDeleteImage = async () => {
+  const onDeleteImage = useCallback(async () => {
     onChangeProfileImage(user.image); // 초기 값으로 변경
-  };
+  }, [onChangeProfileImage, user.image]);
 
   const onEditSubmit = async () => {
     try {
@@ -110,7 +120,8 @@ function ProfileEditForm() {
   return (
     <ProfileEditFormBlock>
       <ProfileImageForm
-        form={form}
+        profileImage={profileImage}
+        imageFileName={imageFileName}
         onChangeProfileImage={onChangeProfileImage}
         onDeleteImage={onDeleteImage}
         onImageUpload={onImageUpload}
