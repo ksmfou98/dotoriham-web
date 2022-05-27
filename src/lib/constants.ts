e8b4db506dfacd2b77c0eae910bca57b9ddaa7e4
ksmfou98 <ksmfou98@naver.com>
2022-05-27 23:07:26 +0900

    fix: env 추가

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,3 +1,2 @@
 export const SERVER_URL = process.env.REACT_APP_API_URL;
-export const GOOGLE_CLIENT_ID =
-  "291594006814-9tmu29asficp56tn7q8ronumev1s39ru.apps.googleusercontent.com";
+export const GOOGLE_CLIENT_ID = process.env.REACT_APP_GOOGLE_CLIENT_ID || "";
