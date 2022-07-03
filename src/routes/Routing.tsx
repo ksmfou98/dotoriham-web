ae87f5a38c810d7cf1d711f9449dde8b8561dd70
ksmfou98 <ksmfou98@naver.com>
2022-07-03 17:45:54 +0900

    refactor: 라우팅 페이지 lazy 로딩으로 변경

--- src/routes/Routing.tsx
+++ src/routes/Routing.tsx
@@ -1,48 +1,53 @@
-import React from "react";
-import DotoriPage from "pages/DotoriPage";
-import LoginPage from "pages/LoginPage";
-import MyPage from "pages/MyPage";
-import NotFoundPage from "pages/NotFoundPage";
-import SearchPage from "pages/SearchPage";
-import TrashPage from "pages/TrashPage";
+import React, { lazy, Suspense } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import Path from "./Path";
 import SidebarRouteLayout from "modules/sidebar/SidebarRouteLayout";
-import ProfileEditPage from "pages/ProfileEditPage";
-import SignupPage from "pages/SignupPage";
 import { useCheckLogin } from "hooks";
-import SharePage from "pages/SharePage";
+
+const SharePage = lazy(() => import("modules/share/pages/SharePage"));
+const ProfileEditPage = lazy(() => import("pages/ProfileEditPage"));
+const SignupPage = lazy(() => import("pages/SignupPage"));
+const LoginPage = lazy(() => import("pages/LoginPage"));
+const MyPage = lazy(() => import("pages/MyPage"));
+const NotFoundPage = lazy(() => import("pages/NotFoundPage"));
+const TrashPage = lazy(() => import("pages/TrashPage"));
+const SearchPage = lazy(() => import("pages/SearchPage"));
+const DotoriPage = lazy(() => import("pages/DotoriPage"));
 
 export function PublicRouting() {
   return (
-    <Routes>
-      <Route path={Path.SharePage} element={<SharePage />} />
-      <Route path={Path.LoginPage} element={<LoginPage />} />
-      <Route path={Path.SignupPage} element={<SignupPage />} />
-      <Route path="*" element={<Navigate replace to={Path.LoginPage} />} />
-    </Routes>
+    <Suspense fallback={<div css="min-height: 100vh" />}>
+      <Routes>
+        <Route path={Path.SharePage} element={<SharePage />} />
+        <Route path={Path.LoginPage} element={<LoginPage />} />
+        <Route path={Path.SignupPage} element={<SignupPage />} />
+        <Route path="*" element={<Navigate replace to={Path.LoginPage} />} />
+      </Routes>
+    </Suspense>
   );
 }
 
 export function PrivateRouting() {
   useCheckLogin();
   return (
-    <Routes>
-      <Route path={Path.SharePage} element={<SharePage />} />
-      <Route
-        path={Path.HomePage}
-        element={<Navigate replace to={Path.DotoriPage} />}
-      />
-      <Route path={Path.MyPage} element={<MyPage />} />
-      <Route path={Path.ProfileEditPage} element={<ProfileEditPage />} />
+    <Suspense fallback={<div css="min-height: 100vh" />}>
+      <Routes>
+        <Route path={Path.SharePage} element={<SharePage />} />
+        <Route
+          path={Path.HomePage}
+          element={<Navigate replace to={Path.DotoriPage} />}
+        />
+        <Route path={Path.MyPage} element={<MyPage />} />
+        <Route path={Path.ProfileEditPage} element={<ProfileEditPage />} />
 
-      <Route path={Path.HomePage} element={<SidebarRouteLayout />}>
-        <Route path={Path.DotoriPage} element={<DotoriPage />} />
-        <Route path={Path.DotoriFolderPage} element={<DotoriPage />} />
-        <Route path={Path.TrashPage} element={<TrashPage />} />
-        <Route path={Path.SearchPage} element={<SearchPage />} />
-      </Route>
-      <Route path="*" element={<NotFoundPage />} />
-    </Routes>
+        <Route path={Path.HomePage} element={<SidebarRouteLayout />}>
+          <Route path={Path.DotoriPage} element={<DotoriPage />} />
+          <Route path={Path.DotoriFolderPage} element={<DotoriPage />} />
+          <Route path={Path.TrashPage} element={<TrashPage />} />
+          <Route path={Path.SearchPage} element={<SearchPage />} />
+        </Route>
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </Suspense>
   );
 }
